05c572e351d24546df34da2e28e0c6abdb4ff6b2
Nicklous J. Grant <nick.julian.grant@gmail.com>
2025-05-06 14:03:58 -0400

    big commit

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
+vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 vim.opt.shiftwidth = 4
+
+vim.opt.showmode = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.keymap.set('i', 'jk', '<Esc>')
 
